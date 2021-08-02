853614d030a0c9f4de8d64446deb999de6433d22
etene <etene@DESKTOP-C8E99GI.myfiosgateway.com>
2021-08-02 11:08:42 -0400

    Project added to git

---
+++ E-RentHouse/src/main/java/cogent/capstone/ERentHouseApplication.java
@@ -0,0 +1,13 @@
+package cogent.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERentHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERentHouseApplication.class, args);
+	}
+
+}
